17522dee513620761dc2383e11e48fcb0003d6f3
amir__jn <52361065+amirjelodarian@users.noreply.github.com>
2023-10-18 01:06:50 +0330

    start mvc dotnet
    this is a begin of dotnet and change header and footer for learning anymore

---
+++ Amir-Jelodarian-Dotnet/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Amir_Jelodarian_Dotnet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
