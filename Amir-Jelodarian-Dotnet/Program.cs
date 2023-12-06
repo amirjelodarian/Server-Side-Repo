bd664e47e831721ae68d6ba874c98a0d7488b786
amir__jn <52361065+amirjelodarian@users.noreply.github.com>
2023-12-07 01:24:52 +0330

    tamrin 5
    doned

--- Amir-Jelodarian-Dotnet/Program.cs
+++ Amir-Jelodarian-Dotnet/Program.cs
@@ -1,7 +1,11 @@
+using Amir_Jelodarian_Dotnet.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<Context>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("Con"));
 
 var app = builder.Build();
 
